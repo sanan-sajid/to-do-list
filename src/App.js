381268c86c10e8358de30c12df26fb8a5517b734
sanan-sajid <sanansajid015@gmail.com>
2023-01-16 01:50:56 +0530

    added little bit of css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "react-bootstrap";
 
 function App() {
   const [toDoList, setToDoList] = useState([]);
@@ -28,20 +29,34 @@ function App() {
     <div className="App">
       <div className="addTask">
         <input
+          className="form-control"
           type="text"
           value={inputValue}
           onChange={handleChange}
           onKeyPress={handleKeyPress}
         />
-        <button onClick={addTask}>Add Task</button>
+        <button className="btn btn-success" onClick={addTask}>
+          Add Task
+        </button>
       </div>
 
       <div className="list">
         {toDoList.map((task, key) => {
           return (
-            <div>
-              <h1 key={key}>{task}</h1>
-              <button onClick={() => handleDelete(key)}>X</button>
+            <div className="d-flex">
+           <div className="text-center">
+              <h1 className="task" key={key}>
+                {key + 1}.{task}
+              </h1>
+</div>
+              <button
+              
+                className="btn btn-danger ml-2"
+                onClick={() => handleDelete(key)}
+              >
+                X
+              </button>
+          
             </div>
           );
         })}
